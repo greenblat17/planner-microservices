365788545161059d4c63230281ce5ba60a514cd5
greenblat17 <leftemail17@mail.ru>
2022-09-12 17:00:01 +0300

    add gateway

---
+++ planner-gateway/src/test/java/com/greenblat/micro/plannergateway/PlannerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenblat.micro.plannergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
