dab3f444914a808c6ad428c362ae3465b6aa8bdd
greenblat17 <leftemail17@mail.ru>
2022-09-13 11:54:55 +0300

    add module for user

---
+++ planner-users/src/test/java/com/greenblat/micro/plannerusers/PlannerUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenblat.micro.plannerusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
