d4b9498df434e5d71892de4919c4f42182e37802
greenblat17 <leftemail17@mail.ru>
2022-09-15 14:26:53 +0300

    add RestTemplate with get request to planner-users

---
+++ planner-utils/src/main/java/com/greenblat/micro/plannerutils/rest_template/UserRestBuilder.java
@@ -0,0 +1,33 @@
+package com.greenblat.micro.plannerutils.rest_template;
+
+import com.greenblat.micro.plannerentity.entity.User;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class UserRestBuilder {
+
+    private static final String BASE_URL = "http://localhost:8765/planner-users/user";
+
+    public boolean userExists(Long userId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        HttpEntity request = new HttpEntity(headers);
+
+
+        ResponseEntity<User> response = null;
+
+        try {
+            response = restTemplate.exchange(BASE_URL + "/show?user_id={userId}", HttpMethod.GET, request, User.class, userId);
+
+            if (response.getStatusCode() == HttpStatus.OK)
+                return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
