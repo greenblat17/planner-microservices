df8910e42478377211165468e36bde5bd9961093
greenblat17 <leftemail17@mail.ru>
2022-09-16 08:47:23 +0300

    add WebClient with request to planner-users for get user by id

--- planner-utils/src/main/java/com/greenblat/micro/plannerutils/rest/webclient/UserWebClientBuilder.java
+++ planner-utils/src/main/java/com/greenblat/micro/plannerutils/rest/webclient/UserWebClientBuilder.java
@@ -1,2 +1,32 @@
-package com.greenblat.micro.plannerutils.rest.webclient;public class UserWebClientBuilder {
+package com.greenblat.micro.plannerutils.rest.webclient;
+
+import com.greenblat.micro.plannerentity.entity.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@Component
+public class UserWebClientBuilder {
+
+    private static final String BASE_URL = "http://localhost:8765/planner-users/user";
+
+    public boolean userExists(Long userId) {
+
+        try {
+            Flux<User> user = WebClient.create(BASE_URL)
+                    .get()
+                    .uri(uriBuilder -> uriBuilder.path("/show")
+                            .queryParam("user_id", userId)
+                            .build())
+                    .retrieve()
+                    .bodyToFlux(User.class);
+
+            if (user.blockFirst() != null)
+                return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
