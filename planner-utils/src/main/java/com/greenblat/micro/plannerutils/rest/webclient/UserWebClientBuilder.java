d9ea003089e1ec81304014e493a1a88461359768
greenblat17 <leftemail17@mail.ru>
2022-09-17 21:00:07 +0300

    add method that call TestDataController

--- planner-utils/src/main/java/com/greenblat/micro/plannerutils/rest/webclient/UserWebClientBuilder.java
+++ planner-utils/src/main/java/com/greenblat/micro/plannerutils/rest/webclient/UserWebClientBuilder.java
@@ -5,15 +5,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.Flow;
+
 @Component
 public class UserWebClientBuilder {
 
-    private static final String BASE_URL = "http://localhost:8765/planner-users/user";
+    private static final String BASE_URL_USER = "http://localhost:8765/planner-users/user";
+    private static final String BASE_URL_DATA = "http://localhost:8765/planner-todo/data";
 
     public boolean userExists(Long userId) {
 
         try {
-            User user = WebClient.create(BASE_URL)
+            User user = WebClient.create(BASE_URL_USER)
                     .get()
                     .uri(uriBuilder -> uriBuilder.path("/show")
                             .queryParam("user_id", userId)
@@ -32,14 +35,23 @@ public class UserWebClientBuilder {
     }
 
     public Flux<User> userExistAsync(Long userId) {
-        Flux<User> userFlux = WebClient.create(BASE_URL)
+
+        return WebClient.create(BASE_URL_USER)
                 .get()
                 .uri(uriBuilder -> uriBuilder.path("/show")
                         .queryParam("user_id", userId)
                         .build())
                 .retrieve()
                 .bodyToFlux(User.class);
+    }
 
-        return userFlux;
+    public Flux<Boolean> initUserData(Long userId) {
+        return WebClient.create(BASE_URL_DATA)
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/init/{id}")
+                        .build(userId))
+                .retrieve()
+                .bodyToFlux(Boolean.class);
     }
 }
